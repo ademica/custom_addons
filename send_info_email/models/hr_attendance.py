bdc32ac4fb00b08d0b4e9c311a28987362128688
adema <adema.kuduzovic@netitup.it>
2025-05-16 07:27:37 +0000

    fixes regarding context timezone

--- send_info_email/models/hr_attendance.py
+++ send_info_email/models/hr_attendance.py
@@ -38,56 +38,12 @@ class HrAttendance(models.Model):
                 
                 else: continue
 
-
                 if template:
-                    template.send_mail(record.id, force_send=True)
 
+                    check_in_str = fields.Datetime.context_timestamp(record, record.check_in).strftime("%d/%m/%Y %H:%M:%S") if record.check_in else ''
+                    check_out_str = fields.Datetime.context_timestamp(record, record.check_out).strftime("%d/%m/%Y %H:%M:%S") if record.check_out else ''
 
-
-            #print("test mail sent")
-            #template = self.env.ref('send_info_email.attendance_email_template')
-            #template.send_email(record, )
-            #self.env['email_template'].search()
-            #pass
-
-            # 1) 3 cases of different templates
-        
-            # employee = record.employee_id
-            # email = employee.work_email 
-            # current_user = self.env.user.name
-
-            # if record.check_in: 
-            #     check_in_local = fields.Datetime.context_timestamp(record, record.check_in)
-            #     formatted_check_in = check_in_local.strftime("%d/%m/%Y %H:%M:%S") 
-
-            # if record.check_out:
-            #     check_out_local = fields.Datetime.context_timestamp(record, record.check_out)
-            #     formatted_check_out = check_out_local.strftime("%d/%m/%Y %H:%M:%S")
-
-                
-            # if employee.receive_email:
-
-            #     subject = f"Attendance {action.capitalize()} Notification"
-            #     message = f"""
-            #     <p>Hello {employee.name},</p>
-                    
-            #     <p>Your attendance record with the following data:</p> 
-                
-            #     <ul>
-            #     {f"<li>Checked in at {formatted_check_in}</li>" if record.check_in else ""}
-            #     {f"<li>Checked out at {formatted_check_out}</li>" if record.check_out else ""}
-            #     </ul>
-
-            #     <p>has been <strong>{action}</strong> by {current_user}</p>
-                    
-            #     <p>Regards,
-            #     <br>HR Team</p>
-            #     """
-
-            #     mail_values = {
-            #         'subject': subject,
-            #         'body_html': f'<p>{message}</p>',
-            #         'email_to': email,
-            #         'email_from': 'adema.sarajlija@gmail.com'
-            #     }
-            #     self.env['mail.mail'].create(mail_values).send()
+                    template.with_context({
+                        'check_in_str': check_in_str,
+                        'check_out_str': check_out_str,
+                    }).send_mail(record.id, force_send=True)
