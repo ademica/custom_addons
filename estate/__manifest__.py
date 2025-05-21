c3afca245ec3c97d58577b862832881f9ccc2dbc
adema <adema.kuduzovic@netitup.it>
2025-05-21 15:22:25 +0000

    working on estate module: adding views(menus), constraints

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,9 @@
 
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'views/estate_property_views.xml',
+        
     ],
 
     'license': 'OPL-1', 
--- estate/models/real_estate.py
+++ estate/models/real_estate.py
@@ -4,13 +4,13 @@ class estateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, default="Unknown")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -21,4 +21,13 @@ class estateProperty(models.Model):
             ('south', 'South'),
             ('east', 'East'),
             ('west', 'West'),
-        ], 'Garden Orientation', default='other')
+        ], 'Garden Orientation')
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer received'),
+        ('offer_accepted', 'Offer accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
+    ],'State', required=True,copy=False,default="new")
+    active = fields.Boolean(default=True)
