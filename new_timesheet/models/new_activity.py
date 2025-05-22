98126b1113778067286108b1c1a3b089a1a88601
adema <adema.sarajlija@gmail.com>
2025-05-22 09:58:48 +0000

    changes on models, data, security

--- new_timesheet/models/new_activity.py
+++ new_timesheet/models/new_activity.py
@@ -1,14 +1,18 @@
-from odoo import fields,models
+from odoo import api, fields, models
 
 class newActivity(models.Model):
     _name = "new_timesheet.activity"
     _description = "Activities from eSolver"
 
-    short_code = fields.Char(required = True)
-    description = fields.Text(required = True)
-
-    _rec_name = "description"
-
     _sql_constraints = [
         ('unique_short_code', 'unique(short_code)', 'The short code must be unique.')
     ]
+
+    name = fields.Char(compute="_compute_name", store="True")
+    short_code = fields.Char(required = True, string="Code")
+    description = fields.Text(required = True)
+
+    @api.depends("short_code", "description")
+    def _compute_name(self):
+        for record in self:
+            record.name = f"{record.short_code} - {record.description}"
